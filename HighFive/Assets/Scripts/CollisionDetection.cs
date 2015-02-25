9e7c12d8b769c4502654a8e30bab1a685a1ed581
ShaneCoates <shane.coates@hotmail.com>
2015-02-25 12:21:46 +1100

    Adding controls to box

---
+++ HighFive/Assets/Scripts/CollisionDetection.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionDetection : MonoBehaviour {
+	bool dissapearing = false;
+	Color colour = new Color(1, 1, 1);
+	Vector3 direction;
+	// Use this for initialization
+	void Start () 
+	{
+		direction =  this.transform.position;
+		direction.Normalize ();
+		direction *= 3;
+		direction.z = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		this.transform.position += direction * Time.deltaTime;
+		if (dissapearing) {
+			colour.a = colour.a - Time.deltaTime;
+			
+			if(colour.a <= 0f)
+				OnBecameInvisible();
+		}
+
+		this.renderer.material.color = colour;
+	}
+
+	void OnMouseOver()
+	{
+		if (Input.GetMouseButton (0)) {
+			dissapearing = true;
+
+		}
+	}
+
+	void OnBecameInvisible()
+	{
+		GameObject.Destroy(gameObject);
+	}
+
+
+}
---
+++ HighFive/Assets/Scripts/Instantiator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Instantiator : MonoBehaviour {
+	public GameObject obj;
+	float timer;
+	Vector3 pos;
+	// Use this for initialization
+	void Start () {
+		Instantiate (obj);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer >= 0.5f) {
+			pos = new Vector3(Random.Range (-0.1f, 0.1f), Random.Range (-0.0f, 0.1f), 0);
+			Instantiate (obj, pos, new Quaternion());
+			timer = 0f;
+		}
+
+	}
+}
