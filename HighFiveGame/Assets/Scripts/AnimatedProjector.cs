a132cd73cb3e5965a03350358124c17c12864c04
ShaneCoates <shane.coates@hotmail.com>
2015-03-18 11:46:51 +1100

    SkyBox
    Removed terrain, as no textures yet. Still saved in prefabs.
    Added skybox with shitty google-images sky for now

--- HighFiveGame/Assets/Scripts/AnimatedProjector.cs
+++ HighFiveGame/Assets/Scripts/AnimatedProjector.cs
@@ -3,14 +3,14 @@ using System.Collections;
 public class AnimatedProjector : MonoBehaviour { 
 	public float fps = 30.0f; 
 	public Texture2D[] frames;
-		
+	public int speed = 10;
 	private int frameIndex;
 	private Projector projector;
 
 	void Start() {
 		projector = GetComponent<Projector>();
 		NextFrame();
-		InvokeRepeating("NextFrame", 1 / fps, 10 / fps);
+		InvokeRepeating("NextFrame", 1 / fps, speed / fps);
 	}
 
 	void NextFrame() {
