27e9ea8a4bcb3f61e1ea7c59df4c07ee88444898
ShaneCoates <shane.coates@hotmail.com>
2015-03-18 11:38:46 +1100

    Terrain
    Created Terrain object and added ability to animate textures in it

---
+++ HighFiveGame/Assets/Scripts/AnimatedProjector.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+public class AnimatedProjector : MonoBehaviour { 
+	public float fps = 30.0f; 
+	public Texture2D[] frames;
+		
+	private int frameIndex;
+	private Projector projector;
+
+	void Start() {
+		projector = GetComponent<Projector>();
+		NextFrame();
+		InvokeRepeating("NextFrame", 1 / fps, 10 / fps);
+	}
+
+	void NextFrame() {
+		projector.material.SetTexture("_MainTex", frames[frameIndex]);
+		frameIndex = (frameIndex + 1) % frames.Length;
+		Debug.Log((frameIndex + 1) % frames.Length);
+	}
+}
