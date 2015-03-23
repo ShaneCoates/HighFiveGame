1f74f22ba8fb2a3ddd493dded8b07bbe69cde2fb
ShaneCoates <shane.coates@hotmail.com>
2015-03-23 14:06:40 +1100

    Headbob
    Added headbobbing effect to camera

---
+++ HighFiveGame/Assets/Scripts/CameraBob.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBob : MonoBehaviour {
+
+	private float timer = 0.0f;
+	float bobbingSpeed = 0.18f;
+	float bobbingAmount = 0.1f;
+	float midpoint = 2.0f;
+	
+	void Update () {
+		float waveslice = 0.0f;
+		
+		Vector3 cSharpConversion = transform.localPosition; 
+		waveslice = Mathf.Sin(timer);
+		timer = timer + bobbingSpeed;
+		if (timer > Mathf.PI * 2) {
+			timer = timer - (Mathf.PI * 2);
+		}
+		if (waveslice != 0) {
+			float translateChange = waveslice * bobbingAmount;
+			translateChange = translateChange;
+			cSharpConversion.y = midpoint + translateChange;
+		}
+		else {
+			cSharpConversion.y = midpoint;
+		}
+		
+		transform.localPosition = cSharpConversion;
+	}
+}
--- HighFiveGame/Assets/Scripts/SplashScreen.cs
+++ HighFiveGame/Assets/Scripts/SplashScreen.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using UnityEngine.UI;
 public class SplashScreen : MonoBehaviour {
 	public Image splashScreen;
-	float timer = 1.5f;
 	// Use this for initialization
 	void Awake () {
 	
@@ -11,14 +10,6 @@ public class SplashScreen : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		timer -= Time.deltaTime;
-		if(timer <= 0f) {
-			PlayGame ();
-		}
-		if (timer <= 1f) {
-			float alpha = Mathf.Lerp (0, 1, timer);
-			splashScreen.color = new Color (1, 1, 1, alpha);
-		}
 		#if UNITY_STANDALONE || UNITY_WEBPLAYER || UNITY_EDITOR
 		if (Input.GetMouseButtonDown (0)) {
 			PlayGame();
